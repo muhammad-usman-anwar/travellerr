b411f4c1900b101e300609cbf4c7156bd3c4915f
Usman <sp15-bse-007@cuilahore.edu.pk>
2019-01-05 00:19:54 +0500

    chat

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,16 +1,61 @@
 const { validationResult } = require("express-validator/check");
 
+const IO = require("../socket");
+
 const Chat = require("../models/chat");
 const User = require("../models/user");
 
-exports.list = (req, res, next) => {
-  Chat.find({
-    users: req.userId
-  })
-    .then(chatDocs => {
-      if (!chatDocs) res.status(401).json({ message: "No chats of the user" });
+exports.list = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  try {
+    const chatList = [];
+    const chatDocs = await Chat.find({
+      users: req.userId
+    });
+    for (let index = 0; index < chatDocs.length; index++) {
+      const doc = chatDocs[index];
+      for (let i = 0; i < doc.users.length; i++) {
+        const user = doc.users[i];
+        if (user != req.userId) {
+          const userDoc = await User.findById(user);
+          chatList.push({
+            id: doc._id,
+            user: {
+              id: userDoc._id,
+              name: `${userDoc.firstName} ${userDoc.lastName}`
+            }
+          });
+        }
+      }
+    }
+    res.status(200).json({ list: chatList });
+  } catch (error) {
+    if (!error.statusCode) error.statusCode = 500;
+    next(error);
+  }
+};
+
+exports.read = (req, res, next) => {
+  const io = IO.getIO();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  Chat.findOne({ _id: req.params.id })
+    .then(chatDoc => {
+      if (!chatDoc) res.status(401).json({ message: "Invalid chat id" });
       else {
-        res.status(200).json({ list: chatDocs });
+        IO.setRoomEvent(chatDoc._id);
+        res.status(200).json({ chat: chatDoc });
       }
     })
     .catch(err => {
@@ -19,19 +64,15 @@ exports.list = (req, res, next) => {
     });
 };
 
-exports.read = (req, res, next) => {
-  Chat.findOne({ _id: req.params.id })
-    .then(chatDoc => {
-      if (!chatDoc) res.status(401).json({ message: "Invalid chat id" });
-      else res.status(200).json({ chat: chatDoc.messages });
-    })
-    .catch(err => {
-      if (!err.statusCode) err.statusCode = 500;
-      next(err);
-    });
-};
-
 exports.insert = (req, res, next) => {
+  const io = IO.getIO();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
   Chat.findOne({ _id: req.body.chatId })
     .then(chatDoc => {
       if (!chatDoc) res.status(401).json({ message: "Invalid chat id" });
@@ -41,6 +82,9 @@ exports.insert = (req, res, next) => {
           message: req.body.message
         });
         chatDoc.save();
+        io.to(req.body.chatId).emit("chat-notify", {
+          data: "message recieved"
+        });
         res.status(200).json({ error: false });
       }
     })
@@ -58,16 +102,27 @@ exports.add = (req, res, next) => {
     error.data = errors.array();
     throw error;
   }
-  const chat = new Chat({
-    users: [req.userId, req.body.userId],
-    messages: [
-      {
-        userId: req.userId,
-        message: req.body.message
+
+  Chat.findOne({ users: req.body.userId })
+    .countDocuments()
+    .then(num => {
+      if (num) {
+        const error = new Error("Chat Allready exists");
+        error.statusCode = 400;
+        error.data = errors.array();
+        throw error;
+      } else {
+        return new Chat({
+          users: [req.userId, req.body.userId],
+          messages: [
+            {
+              userId: req.userId,
+              message: req.body.message
+            }
+          ]
+        }).save();
       }
-    ]
-  })
-    .save()
+    })
     .then(result => {
       res.status(201).json({
         message: "New chat created",
--- socket.js
+++ socket.js
@@ -1,14 +1,25 @@
+const EventEmitter = require("events");
+
 let io;
+const ioEvents = new EventEmitter();
 
 module.exports = {
   init: httpServer => {
     io = require("socket.io")(httpServer);
+    io.on("connection", socket => {
+      ioEvents.on("addToRoom", room => {
+        socket.join(room);
+      });
+    });
     return io;
   },
   getIO: () => {
     if (!io) {
-      throw new Error("Socket.o is initialized");
+      throw new Error("Socket.io is not initialized");
     }
     return io;
+  },
+  setRoomEvent: room => {
+    ioEvents.emit("addToRoom", room);
   }
 };
