e0a87d8bc6cd9d4b0bf4982b94c81222a19ba04f
Usman <sp15-bse-007@cuilahore.edu.pk>
2019-01-22 14:42:48 +0500

    started trip module: 22/1/2019

--- controllers/trip.js
+++ controllers/trip.js
@@ -1,7 +1,59 @@
-const Trip = require("../models/trip");
+const {
+    validationResult
+} = require("express-validator/check");
 
-exports.create = (req, res, next) => {};
+const Trip = require("../models/trip");
+const Post = require('../models/post')
+
+exports.create = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error("Validation Failed");
+            error.statusCode = 422;
+            error.data = errors.array();
+            throw error;
+        }
+        const postDoc = await Post.findById(req.body.postId)
+        if (!postDoc) throw new Error('Internal server error')
+        new Trip({
+                users: [req.userId],
+                carId: req.body.carId,
+                postId: postDoc._id,
+                chatId: req.body.chatId
+            })
+            .save()
+            .then(result => {
+                res.status(201).json({
+                    error: false,
+                    message: 'trip created'
+                }).catch(err => {
+                    console.log(err);
+                    const error = new Error('Internal server error')
+                    error.statusCode = 401
+                    throw error
+                })
+            })
+    } catch (error) {
+        if (!error.statusCode) error.statusCode = 500
+        next(error)
+    }
+};
 
 exports.start = (req, res, next) => {};
 
 exports.finish = (req, rees, next) => {};
+
+exports.getTrip = async (req, res, next) => {
+    try {
+        const trip = await Trip.findById(req.params.id)
+        if (!trip) throw new Error('Invalid Trip ID')
+        res.status(200).json({
+            error: false,
+            trip: trip
+        })
+    } catch (error) {
+        if (!error.statusCode) error.statusCode = 500
+        next(error)
+    }
+}
